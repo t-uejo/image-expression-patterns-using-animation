012acf04b99ccc2c1165168a42526498b99ebffa
uejo <info@taikiuejo.com>
2023-05-30 08:00:43 +0900

    feat：loadingを見せるためにsetTimeoutを実装

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,4 +1,7 @@
 window.onload = () => {
   const loader = document.querySelector("#loading-wrapper");
-  loader.classList.add("completed");
+
+  setTimeout(() => {
+    loader.classList.add("completed");
+  }, 1500);
 };
