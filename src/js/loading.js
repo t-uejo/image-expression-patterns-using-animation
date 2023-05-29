49332c7dc542972b956bd0b4d75b85b992df99a0
uejo <info@taikiuejo.com>
2023-05-30 08:04:12 +0900

    feat；DOMがリロードされてからでいいかも

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,7 +1,15 @@
-window.onload = () => {
+// window.onload = () => {
+//   const loader = document.querySelector("#loading-wrapper");
+
+//   setTimeout(() => {
+//     loader.classList.add("completed");
+//   }, 1500);
+// };
+
+document.addEventListener("DOMContentLoaded", function () {
   const loader = document.querySelector("#loading-wrapper");
 
   setTimeout(() => {
     loader.classList.add("completed");
   }, 1500);
-};
+});
